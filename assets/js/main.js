5366a9566eb792e4d1e088075f506d2130e2df35
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-09-06 16:00:54 +0200

    Add shadow to gallery section.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function() {
+    "use strict";
+  
+    var macy = Macy({
+        container: '#macy-container',
+        trueOrder: false,
+        waitForImages: false,
+        margin: 24,
+        columns: 3,
+        breakAt: {
+            1200: 3,
+            940: 3,
+            520: 2,
+            400: 1
+        }
+    });
+})()
